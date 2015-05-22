676b7c26884d6fc61859e6de4ba8e159c78bfde2
Gerold31 <kramer.leroy@gmail.com>
2015-05-22 22:36:30 +0200

    add main.ino to pack

--- pack.sh
+++ pack.sh
@@ -1,7 +1,9 @@
-DIRS=('prototype1' 'plot' 'tools')
+DIRS=('prototype1' 'plot' 'tools' 'hardware/main.ino')
 PACK="pack.zip"
 
 cd "$(dirname "$0")"
+
+cp hardware/servoctl-device/main.ino hardware/main.ino
 
 rm "$PACK"
 for DIR in "${DIRS[@]}"
@@ -9,3 +11,5 @@ do
 	git clean -Xdi "$DIR"
 	zip -r "$PACK" "$DIR" -x "**/"
 done
+
+rm hardware/main.ino
